1e9476609687a1aa8e91037124ac6b7d670cb45b
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-03 22:59:48 +0200

    Update Store Provision.js

--- Store Provision.js
+++ Store Provision.js
@@ -10,18 +10,13 @@ function storeProvision(stock, ordered) {
             stock[idx] = Number(stock[idx]) + qtyToAdd;
         }
     }
-
-    
     let stockObject = {};
     for (let i = 0; i < stock.length; i += 2) {
         stockObject[stock[i]] = stock[i + 1];
-    }
-
-    
+    }    
     for (let [product, quantity] of Object.entries(stockObject)) {
         console.log(`${product} -> ${quantity}`);
     }
 }
- 
 storeProvision(['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
                ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']);
